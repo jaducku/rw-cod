54fdd86929eca7e7ed3b17e7958d53be356be586
jaducku <jaducku@kakao.com>
2023-12-20 22:10:27 +0900

    quick fix(home layout)

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -3,18 +3,22 @@ import { authOptions } from "../utils/auth";
 import { getServerSession } from "next-auth";
 import NavBar from "../components/nav-bar";
 
-export default async function HomeLayout({children}:{children: ReactNode}){
-    const session = await getServerSession(authOptions);
-    /*
+export default async function HomeLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+  /*
     if(!session){
         return redirect("/login");
     }
     */
-    return(
-        <>
-            <main className="w-full max-w-8xl mx-auto sm:px-6 lg:px-8">
-                {children}
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main className="w-full max-w-8xl mx-auto sm:px-6 lg:px-8">
+        {children}
+      </main>
+    </>
+  );
 }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,5 @@
-import { Metadata } from "next"
-import Image from "next/image"
+import { Metadata } from "next";
+import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -8,65 +8,34 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Overview } from "./components/overview";
 import { RecentSales } from "./components/recent-sales";
 
 export const metadata: Metadata = {
   title: "Road Warrior",
   description: "Road Warrior",
-}
+};
 
 export default function DashboardPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/dashboard-light.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/dashboard-dark.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden flex-col md:flex">
+      <div className="flex-col sm:flex md:flex">
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">RW DashBoard</h2>
+            <h2 className="text-3xl font-bold tracking-tight">RW Dashboard</h2>
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
             <TabsList>
-              <TabsTrigger value="overview">
-                Overview
-              </TabsTrigger>
-              <TabsTrigger value="analytics">
-                Analytics
-              </TabsTrigger>
-              <TabsTrigger value="reports">
-                Reports
-              </TabsTrigger>
-              <TabsTrigger value="notifications">
-                Notifications
-              </TabsTrigger>
-              <TabsTrigger value="myProfile">
-                My Profile
-              </TabsTrigger>
+              <TabsTrigger value="overview">Overview</TabsTrigger>
+              <TabsTrigger value="analytics">Analytics</TabsTrigger>
+              <TabsTrigger value="reports">Reports</TabsTrigger>
+              <TabsTrigger value="notifications">Notifications</TabsTrigger>
+              <TabsTrigger value="myProfile">My Profile</TabsTrigger>
             </TabsList>
             <TabsContent value="overview" className="space-y-4">
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+              <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
                 <Card>
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">
@@ -104,11 +73,11 @@ export default function DashboardPage() {
                     </CardTitle>
                   </CardHeader>
                   <CardContent>
-                    <div className="text-3xl font-bold">4500 Hour</div>
+                    <div className="text-3xl font-bold">4</div>
                   </CardContent>
                 </Card>
               </div>
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+              <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-7">
                 <Card className="col-span-4">
                   <CardHeader>
                     <CardTitle>병종 현황</CardTitle>
@@ -134,5 +103,5 @@ export default function DashboardPage() {
         </div>
       </div>
     </>
-  )
+  );
 }
